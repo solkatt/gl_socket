7eb8b1df6a3dc809820e21cc72aaca7288587a02
Viktor Ödman <viktor.odman@consid.se>
2023-07-11 13:28:47 +0200

    init

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+import ReactDOM from 'react-dom/client'
+import { Canvas } from  '@react-three/fiber';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+    <>
+    <Canvas />
+        Soon to be a badass R3F application
+    </>
+)
